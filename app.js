9919d39f84e8e21277cbe27237f3014501ec23d6
guangying123 <1242503370@qq.com>
2017-06-06 11:30:59 +0800

    西电学籍管理中期版

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 var Homepage = require('./routes/Homepage');
 var Homepage1 = require('./routes/Homepage1');
-var personal = require('./routes//personal');
+var personal = require('./routes/personal');
+var Center_back = require('./routes/Center_back');
 
 var app = express();
 // view engine setup
@@ -40,6 +42,7 @@ app.use('/users', users);
 app.use('/Homepage',Homepage);//主界面 学生
 app.use('/Homepage1',Homepage1);//主界面  老师
 app.use('/personal',personal);
+app.use('/Center_back',Center_back);
 
 
 app.get('/destroy',function (req,res) {
@@ -57,34 +60,48 @@ app.get('/destroy',function (req,res) {
 });
 
 //提交处理路由
-app.post('/loginroute',function (req,res) {
-    var temuserid = "14030130040";
-    var temppw = "chang199698";
-    var tempkind = "学生";
-    var tempkind1 = "老师";
+app.post('/loginroute',function (req,res)
+{
+    var client = mysql.createConnection({
+        host:'127.0.0.1',
+        user:'root',
+        database:'XD_School_Manager',
+        password:'123456',
+        useConnectionPooling: true
+    });
     var userid = req.body.userid;
     var pw = req.body.pw;
     var kind = req.body.kind;
-    console.log(userid);//用户名
-    console.log(pw);//密码
-    console.log(kind);//类型
-    if(userid==temuserid && pw==temppw && kind==tempkind)//最终改成从数据库中查询验证  判断为学生
+    if(kind=="学生")//最终改成从数据库中查询验证  判断为学生
     {
-      req.session.userid = userid;
-      req.session.pw = pw;
-      req.session.kind=kind;
-      res.end("student");
+        var sqlstudent = "select * from user_pw where userid=? and pw=?";
+        var studentpar = [userid,pw];
+        client.query(sqlstudent,studentpar,function (err,result) {
+            if(err)
+            {
+                console.log(err);
+                res.end("false");//检验失败
+            }
+            else
+            {
+                if(result.length!=0)
+                {
+                    req.session.userid = userid;
+                    req.session.pw = pw;
+                    req.session.kind=kind;
+                    //console.log(result);
+                    res.end("student");
+                }
+            }
+        })
     }
-    else
+    else  //类型为老师  判断方式和学生类似
     {
-        if(userid==temuserid && pw==temppw && kind==tempkind1)//判断为老师
-        {
             req.session.userid = userid;
             req.session.pw = pw;
             req.session.kind = kind;
             res.end("teacher");
-        }
-        res.end("false");
+
     }
 });
 
